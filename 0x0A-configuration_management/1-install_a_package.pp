88f42c17c9c9f3d2c52ae4bfb28cda2f9d6c6cfa
oluwaseunT <taiwooluwaseun658@gmail.com>
2024-08-14 10:49:49 +0100

    Added file edited

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,8 +1,6 @@
 # install flask version 2.1.0 using pip3
-
 package { 'flask':
   ensure   => '2.1.0',
-  provider => 'pip',
-  install_options => ['--pip-command', '/usr/bin/pip3'],
+  provider => 'pip3',
 }
 
